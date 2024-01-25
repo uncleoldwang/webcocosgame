6ea76214bf37083c5f79e7e14491737233620a2e
王叔叔 <35623889+uncleoldwang@users.noreply.github.com>
2024-01-25 18:44:50 +0800

    “新的包”
    “新的包”

--- src/settings.87dbb.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/Login.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"d0832",resources:"21fd0",main:"72844"}};
---
+++ src/settings.88ae6.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/Login.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"d0832",resources:"bd481",main:"d542a"}};
